2eb8d5ace7ddc0917a75dd515c5a9c33610f6039
seoyaon <seoyaon@gmail.com>
2023-01-04 21:43:20 +0900

    기능 구현

--- todo.js
+++ todo.js
@@ -20,6 +20,7 @@ function createTodo () {
     const newBtn = document.createElement('button');
     const newSpan = document.createElement('span');
     const todoInput = document.querySelector('#todoInput');
+    const deleteAll = document.querySelector('.delete-btn-wrap');
 
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
@@ -31,4 +32,37 @@ function createTodo () {
     // console.log(todoList)
 
     todoInput.value = '';
+
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+
+        saveItemsFn();
+    });
+
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+    });
+
+    saveItemsFn();
 };
+
+function deleteAll() { // 전체 삭제 버튼
+    const liList = document.querySelectorAll('#todoList li');
+    // console.log(liList[0])
+    for ( let i = 0; i < liList.length; i++){
+        liList[i].remove();
+        // console.log(liList[i])
+    }
+}
+
+function saveItemsFn () { // 로컬에 데이터 저장하기
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++){
+        const todoObj = {
+            contents: todoList.children[i].querySelector('span').textContent,
+            complete: todoList.children[i].classList.contains('complete')
+        };
+        saveItems.push(todoObj);
+    }
+    console.log(JSON.stringify(saveItems))
+}
