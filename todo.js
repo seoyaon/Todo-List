2d750c109422b7025c9422b7f07caa3cbac18ac3
seoyaon <seoyaon@gmail.com>
2023-01-05 21:59:37 +0900

    localstorage 기능 구현

--- todo.js
+++ todo.js
@@ -1,6 +1,16 @@
+const todoInput = document.querySelector('#todoInput');
 const addBtn = document.querySelector('#addBtn');
 
-function keyCodeCheck () {
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+console.log(savedTodoList)
+
+if (savedTodoList) { // 로컬에서 데이터 가져오기
+    for(let i = 0; i < savedTodoList.length; i++){
+        createTodo(savedTodoList[i])
+    }
+}
+
+function keyCodeCheck () { // 엔터키로 추가
 	// console.log(window.event)
 
 	if(window.event.keyCode === 13 && todoInput.value !== ''){
@@ -8,40 +18,50 @@ function keyCodeCheck () {
     }
 }
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', () => { // + 버튼으로 추가
     if(todoInput.value !== ''){
         createTodo();
     }
 });
 
-function createTodo () {
+function createTodo (storageData) { // 할 일 추가 기능
+    let todoContents = todoInput.value;
+    if (storageData) {
+        todoContents = storageData.contents
+    }
+
     const todoList = document.querySelector('#todoList');
     const newLi = document.createElement('li');
     const newBtn = document.createElement('button');
     const newSpan = document.createElement('span');
-    const todoInput = document.querySelector('#todoInput');
     const deleteAll = document.querySelector('.delete-btn-wrap');
 
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     // console.log(newLi)
 
-    newSpan.textContent = todoInput.value;
+    newSpan.textContent = todoContents
 
     todoList.appendChild(newLi);
     // console.log(todoList)
 
     todoInput.value = '';
 
-    newBtn.addEventListener('click', () => {
+    newBtn.addEventListener('click', () => { // 체크박스 클릭
         newLi.classList.toggle('complete');
 
         saveItemsFn();
     });
 
-    newLi.addEventListener('dblclick', () => {
+    newLi.addEventListener('dblclick', () => { // 더블 클릭
         newLi.remove();
+
+        saveItemsFn();
     });
+
+    if (storageData && storageData.complete === true) {
+        newLi.classList.add('complete')
+    }
 
     saveItemsFn();
 };
@@ -53,7 +73,8 @@ function deleteAll() { // 전체 삭제 버튼
         liList[i].remove();
         // console.log(liList[i])
     }
-}
+    saveItemsFn();
+};
 
 function saveItemsFn () { // 로컬에 데이터 저장하기
     const saveItems = [];
@@ -64,5 +85,13 @@ function saveItemsFn () { // 로컬에 데이터 저장하기
         };
         saveItems.push(todoObj);
     }
-    console.log(JSON.stringify(saveItems))
+    // console.log(JSON.stringify(saveItems))
+
+    if (saveItems.length === 0) {
+        localStorage.removeItem('saved-items')
+    }else{
+        localStorage.setItem('saved-items', JSON.stringify(saveItems));
+    }
 }
+
+
